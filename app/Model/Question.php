33fa441fa913e12737dac684a4c4fd889cd6bbd8
saadayouj <saadayouj666@gmail.com>
2020-01-09 18:18:25 +0100

    CRUD For Questions & Question Resource Transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -18,5 +25,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
