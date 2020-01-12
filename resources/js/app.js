1babff1098c9471ee79b8e9bd554ddb816cc11d6
saadayouj <saadayouj666@gmail.com>
2020-01-12 20:15:50 +0100

    Setup Vue Router & Menu/Footer Home Page

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,7 +22,7 @@ Vue.use(Vuetify);
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('AppHome', require('./components/AppHome.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -30,7 +30,10 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+import router from './Router/router.js'
+
 const app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
+    router
 });
---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-toolbar>
+      <v-toolbar-title>Forum F.A</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>Forum</v-btn>
+        <v-btn text>Ask a Question</v-btn>
+        <v-btn text>Create Your Category</v-btn>
+        <v-btn text>Login</v-btn>
+      </v-toolbar-items>
+
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
